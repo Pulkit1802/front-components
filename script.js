28f3dc487fb105ec907c689dd9e1fea562bf47ec
Your Name <pulkitmlk11@gmail.com>
2022-08-05 09:45:37 +0530

    Js functionality, modal window

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let side_nav_opener = document.querySelector('.btn-side-nav');
 let overlay = document.querySelector('.overlay')
+let modal_close_btn = document.querySelector('.close-modal-btn');
+let modal = document.querySelector('.modal');
+let modal_image = document.querySelector('.modal-img img')
+let side_nav = document.querySelector('.side-nav');
+
+let header_images = document.querySelectorAll('.header-img');
+
+
 
 side_nav_opener.addEventListener('click', ()=>{
-    document.querySelector('.side-nav').classList.toggle('hidden');
-    overlay.classList.toggle('hidden')
+    side_nav.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
 })
 
 overlay.addEventListener('click', ()=>{
-    document.querySelector('.side-nav').classList.add('hidden');
+    side_nav.classList.add('hidden');
     overlay.classList.add('hidden');
+    modal.classList.add('hidden');
 })
+
+modal_close_btn.addEventListener('click', ()=> {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden')
+})
+
+header_images.forEach((img => {
+    img.addEventListener('click', (e)=>{
+        console.log(e.target.getAttribute('src'))
+        overlay.classList.remove('hidden');
+        modal.classList.remove('hidden');
+        modal_image.setAttribute('src', `${e.target.getAttribute('src')}`);
+    })
+}))
